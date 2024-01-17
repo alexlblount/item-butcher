99ee1d38de40c72d2d6d5d23094de649d4b02140
Alex Blount <alexlblount+git@gmail.com>
2024-01-16 22:30:44 -0600

    Updates

--- .prettierrc.cjs
+++
@@ -1,9 +0,0 @@
-// https://leandroaps.medium.com/building-a-modern-react-app-with-vite-vitest-typescript-eslint-prettier-and-husky-abc7a9876e9d
-module.exports = {
-  singleQuote: true,
-  semi: true,
-  useTabs: false,
-  tabWidth: 2,
-  trailingComma: 'all',
-  printWidth: 128,
-};
